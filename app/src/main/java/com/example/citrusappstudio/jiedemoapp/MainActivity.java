1d52511828502b9c4cf598046e307bde689ea2b8
Jie He <jie_he@live.com>
2019-08-28 13:47:37 -0400

    Added Action bar (toolbar) and options on it. No functionallity on settings option yet.
    Signed-off-by: Jie He <jie_he@live.com>

--- app/src/main/java/com/example/citrusappstudio/jiedemoapp/MainActivity.java
+++ app/src/main/java/com/example/citrusappstudio/jiedemoapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.citrusappstudio.jiedemoapp;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -26,10 +30,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         mainLayout = findViewById(R.id.main_layout);
         setting = new Setting(4);
 
         createGrid();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            //TODO Settings Activity
+            Snackbar.make(mainLayout, "Switch to settings activity here", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     //TODO place into seperate gridAdapter class
