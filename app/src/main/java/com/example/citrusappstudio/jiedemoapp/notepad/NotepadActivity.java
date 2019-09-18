84180776a8db58da229d1e5bf46a9b1dce1e3dac
Jie He <jie_he@live.com>
2019-09-18 17:53:40 -0400

    add notepad to manifest.
    Signed-off-by: Jie He <jie_he@live.com>

--- app/src/main/java/com/example/citrusappstudio/jiedemoapp/notepad/NotepadActivity.java
+++ app/src/main/java/com/example/citrusappstudio/jiedemoapp/notepad/NotepadActivity.java
@@ -1,4 +1,4 @@
-package com.example.citrusappstudio.jiedemoapp;
+package com.example.citrusappstudio.jiedemoapp.notepad;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class NotePadActivity extends AppCompatActivity {
+import com.example.citrusappstudio.jiedemoapp.R;
+
+public class NotepadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
